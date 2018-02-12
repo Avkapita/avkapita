4cee4659efd7ef142ddc05d204bfea2e5ab646b0
Anna <avkapita@gmail.com>
2018-02-12 03:50:21 +0300

    js2

--- js/menu-accordeon.js
+++ js/menu-accordeon.js
@@ -1,22 +1,77 @@
-var accordion = document.getElementById('menu-accordion');
+var accordion = document.getElementById("menu-accordion"),
+    items = accordion.getElementsByClassName("acc-menu__item"),
+    contents = accordion.getElementsByClassName("acc-menu__content"),
 
-var i;
+     i;
 
 accordion.addEventListener('click', function(e) {
-  //console.log("this");
-   //console.log(this);
-  //console.log("e.target");
-  // console.log(e.target);
-  if (e.target.classList.contains('acc-menu__trigger') ) {
+  if (e.target.classList.contains("acc-menu__trigger") ) {
     var trigger = e.target;
     var content = trigger.nextElementSibling;
-    
-    if (content.style.width) {
-      content.style.width = null;
-    } else {
-      content.style.width = '520px';
-     
+    var item = trigger.parentNode;
+
+
+
+    if (!item.classList.contains("active")) { // не активный
+      // удаляем active для всех li
+      for (i = 0; i < items.length; i++) {
+        items[i].classList.remove("active");
+      }
+      // добавляем active для текущего li
+      item.classList.add("active");
+
+      // закрываем все блоки с контентом
+      for (i = 0; i < contents.length; i++) {
+        contents[i].style.offsetWidth = null;
+      }
+      // открываем текущий блок с контентом
+      content.style.offsetWidth = content.offsetWidth + "px";
+      
+    } else { // активный
+      // удаляем класс active для текущего li
+      item.classList.remove("active");
+      
+      // закрываем текущий блок с контентом
+      content.style.offsetWidth = null;
     }
   }
+});
 
+
+// общие переменные должны отличаться названиями от первого аккордеона
+// переменные внутри обработчика событий могут быть такими же
+var accordion2 = document.getElementById("team-accordion"),
+    items2 = accordion2.getElementsByClassName("team-acc__item"),
+    contents2 = accordion2.getElementsByClassName("team-acc__content"),
+    i;
+
+accordion2.addEventListener("click", function(e) {
+  if (e.target.classList.contains("team-acc__trigger")) {
+    var trigger = e.target;
+    var content = trigger.nextElementSibling;
+    var item = trigger.parentNode;
+
+    if (!item.classList.contains("active")) { // не активный
+      // удаляем active для всех li
+      for (i = 0; i < items2.length; i++) {
+        items2[i].classList.remove("active");
+      }
+      // добавляем active для текущего li
+      item.classList.add("active");
+
+      // закрываем все блоки с контентом
+      for (i = 0; i < contents2.length; i++) {
+        contents2[i].style.height = null;
+      }
+      // открываем текущий блок с контентом
+      content.style.height = content.scrollHeight + "px";
+      
+    } else { // активный
+      // удаляем класс active для текущего li
+      items2.classList.remove("active");
+      
+      // закрываем текущий блок с контентом
+      contents2.style.height = null;
+    }
+  }
 });
--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-const left = document.querySelector("#left");
+/*const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 const items = document.querySelector("#items");
 
@@ -21,7 +21,49 @@ left.addEventListener("click", function(e) {
     currentRight -= step;
     items.style.right = currentRight + "px";
   }
-});
+});*/
   
+var leftArrow = document.querySelector('.arrow-left');
+var rightArrow = document.querySelector('.arrow-right');
+var sliderList = document.querySelector('.slider-list');
+var sliderContainer = document.querySelector('.slider');
+var size = parseInt(getComputedStyle(sliderContainer).width);
+var start = 1;
 
-var  ingr = document.querySelector("#ingredients");
+leftArrow.addEventListener('click', function (e) {
+    e.preventDefault();
+    var currentLeft = (parseInt(getComputedStyle(sliderList).left));
+ 
+    if (start > 1 && currentLeft % size == 0) {        
+      sliderList.style.left = currentLeft + size + 'px';
+      start--;      
+      
+    } else if (currentLeft % size == 0) {
+      sliderList.style.left = currentLeft - 4 * size + 'px';
+      start = 5;      
+    }
+ })
+
+rightArrow.addEventListener('click', function (e) {
+    e.preventDefault();
+    var currentLeft = (parseInt(getComputedStyle(sliderList).left));
+
+    if (start < 5 && currentLeft % size == 0) {
+
+        sliderList.style.left = currentLeft - size + 'px';
+        start++;
+    
+    } else if (currentLeft % size == 0) {
+      sliderList.style.left = 0 + 'px';
+      start = 1;     
+    }
+})
+
+
+window.addEventListener('resize', function() {  
+  size = parseInt(getComputedStyle(sliderContainer).width); 
+  // console.log("size: " + size);
+  // console.log("start: " + start);
+  // console.log(-size * start);
+  sliderList.style.left = -size * (start-1) + 'px';
+});
