c850ac23769dbd69fce467a21b82bdf274a492f1
Anna <avkapita@gmail.com>
2018-02-19 03:29:04 +0300

    slm+ops2

--- js/slidermodal.js
+++ js/slidermodal.js
@@ -2,11 +2,8 @@
 //////SLIDER - MODAL
 var ingredients = document.querySelector(".ingredients");
 
-
 ingredients.addEventListener("click", function(e) {
 
-
- 
   if (e.target.classList.contains("ingredients") || e.target.classList.contains("ingr-title")) { 
   
     var trigger;
@@ -20,22 +17,17 @@ ingredients.addEventListener("click", function(e) {
       console.log(trigger);
     }
     
-    var ingrModal = trigger.nextElementSibling;
-  
-
-    console.log(ingrModal);   
+    var ingrModal = trigger.nextElementSibling;  
     
     
     if (!ingrModal.classList.contains("active")) { 
    
-      
-        ingrModal.classList.add("active");
+      ingrModal.classList.add("active");
       
       
     } else { 
       ingrModal.classList.remove("active");
-      
-      
+       
     
     }
   }
